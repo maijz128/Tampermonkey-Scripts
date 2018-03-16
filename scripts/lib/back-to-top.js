ca381938b54645e734ac3e21df960ccff5db1393
Maijz128 <Maijz128@gmail.com>
2018-03-16 10:55:01 +0800

    back-to-top.js: 改善UI

--- scripts/lib/back-to-top.js
+++ scripts/lib/back-to-top.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Go to Top
+// @name         Back to Top
 // @namespace    https://github.com/maijz128
-// @version      0.1
+// @version      0.2
 // @description  Scroll Back To Top Button
 // @author       MaiJZ
 // @grant        none
@@ -11,8 +11,8 @@
 (function () {
     'use strict';
 
-    const BUTTON_ID = "btnScrollToTop";
-    const TOP_FUNCTION_NAME = "btnScrollToTop_TopFunction";
+    const BUTTON_ID = "btnBackToTop";
+    const TOP_FUNCTION_NAME = "btnBackToTop_TopFunction";
 
     addButtonStyle();
     addFunction();
@@ -21,11 +21,12 @@
     function addButtonStyle() {
         var style = '#' + BUTTON_ID + '{';
         style += 'display:none; position:fixed; bottom:20px; right:30px; z-index:99; ';
-        style += 'font-size:15px; border:0; outline:0; background-color:lightgrey; color:#8590a6; ';
-        style += 'cursor:pointer; padding:10px; border-radius:4px; box-shadow:0 1px 3px rgba(26,26,26,.1); ';
+        style += 'font-size:15px; border:0; outline:0; background-color:#fff; color:#8590a6; ';
+        style += 'cursor:pointer; text-align: center; width: 40px; height: 40px; ';
+        style += 'border-radius:4px; box-shadow:0 1px 3px rgba(26,26,26,.1); ';
         style += '}';
 
-        style += '#' + BUTTON_ID + ':hover { background-color: #555;  }';
+        style += '#' + BUTTON_ID + ':hover { background-color: #d3d3d3;  }';
 
         addStyle(style);
     }
@@ -42,7 +43,7 @@
 
         var strFun = 'function ' + TOP_FUNCTION_NAME + '()';
         // strFun += '{ document.body.scrollTop = 0; document.documentElement.scrollTop = 0;}';
-        strFun += '{ var scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight';
+        strFun += '{ var scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight;';
         strFun += 'var increment = scrollHeight / 100;';
         strFun += 'var gotoTop= function(){ var currentPosition = document.documentElement.scrollTop || document.body.scrollTop;';
         strFun += 'currentPosition -= increment; if(currentPosition>0){window.scrollTo(0,currentPosition)}else{window.scrollTo(0,0);clearInterval(timer);timer = null}}; ';
@@ -54,11 +55,15 @@
     }
 
     function addButton() {
+        var strSVG = '<svg class="" title="Back to Top" fill="#8590a6" viewBox="0 0 24 24" width="24" height="24">';
+        strSVG += '<path d="M16.036 19.59a1 1 0 0 1-.997.995H9.032a.996.996 0 0 1-.997-.996v-7.005H5.03c-1.1 0-1.36-.633-.578-1.416L11.33 4.29a1.003 1.003 0 0 1 1.412 0l6.878 6.88c.782.78.523 1.415-.58 1.415h-3.004v7.005z"></path></svg>';
+
+
         var elButton = document.createElement("button");
         elButton.setAttribute('id', BUTTON_ID);
         elButton.setAttribute("onclick", TOP_FUNCTION_NAME + '()');
-        elButton.setAttribute('title', "Go To Top");
-        elButton.innerHTML = "Top";
+        elButton.setAttribute('title', "Back to Top");
+        elButton.innerHTML = strSVG;
 
         document.body.appendChild(elButton);
     }
