3b6e7da414f30e2221beb668c6f2c4d084910ec8
Maijz128 <Maijz128@gmail.com>
2018-03-21 22:09:43 +0800

    jandan.net.js: 鼠标悬浮至[ShowMore]上自动展开/收起

--- scripts/jandan.net.js
+++ scripts/jandan.net.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name  jandan.net - 煎蛋网
 // @namespace    https://github.com/maijz128
-// @version      0.1.0
-// @description  鼠标悬浮至GIF自动播放。
+// @version      0.2.0
+// @description  鼠标悬浮至GIF自动播放，鼠标悬浮至[ShowMore]上自动展开/收起。
 // @author       MaiJZ
 // @match        *://*.jandan.net/*
 // @grant        none
@@ -14,21 +14,59 @@
 })();
 
 function main() {
-    GIF();
+    setTimeout(function () {
+        GIF();
+        ShowMore();
+    }, 1000);
 }
 
 function GIF() {
+    $(".gif-mask").hover(function () { // mouseenter event
+        $(this).click();
 
-    setTimeout(function () {
-        $(".gif-mask").hover(function () { // mouseenter event
-            $(this).click();
+        var el_acv_comment = $(this).parent().parent();
+        el_acv_comment.click();
+    }, function () {   // mouseleave event
+        // do some thing...
+    });
+}
 
-            var el_acv_comment = $(this).parent().parent() ;
-            el_acv_comment.click();
+function ShowMore_Trigger(hoverTime, callback) {
+    var self = this;
+    this._element = null;
+    this._timeout = null;
+    this._hoverTime = hoverTime;
+    this._callback = callback;
+
+    this.start = function (element) {
+        self.cancel();
+
+        self._element = element;
+        self._timeout = setTimeout(function () {
+            if (self._element !== null && self._callback !== null) {
+                self._callback(self._element);
+            }
+        }, self._hoverTime);
+    };
+
+    this.cancel = function () {
+        self._element = null;
+        clearTimeout(self._timeout);
+    };
+}
+
+function ShowMore() {
+    var trigger = new ShowMore_Trigger(500, function (element) {
+        $(element).click();
+    });
+
+    $('.show_more').each(function (index, element) {
+        $(this).hover(function () { // mouseenter event
+            trigger.start($(this));
         }, function () {   // mouseleave event
-            // do some thing...
+            trigger.cancel();
         });
-    }, 1000);
+    });
 
 }
 
