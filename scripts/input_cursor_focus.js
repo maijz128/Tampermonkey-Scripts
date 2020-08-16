883ad2e1eba869f8fe3e91f451fca75738c66774
maijz128 <maijz128@gmail.com>
2020-08-16 16:54:50 +0800

    add input_cursor_focus.js

---
+++ scripts/input_cursor_focus.js
@@ -0,0 +1,169 @@
+// ==UserScript==
+// @name         Input Cursor Focus - MaiJZ
+// @namespace    https://github.com/maijz128
+// @version      0.1.0
+// @description  描述
+// @author       MaiJZ
+// @match        *://*/*
+// @require      http://code.jquery.com/jquery-1.12.4.min.js
+// @require      https://cdn.bootcss.com/clipboard.js/1.7.1/clipboard.min.js
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_setClipboard
+// @grant        unsafeWindow
+// @grant        window.close
+// @grant        window.focus
+// ==/UserScript==
+
+var ONLY_WHITE_LIST = true;  // 只允许白名单
+var WHITE_LIST = [
+    "baidu.com", "google.com", "taobao.com", "youtube.com",
+];
+
+
+(function () {
+    setTimeout(function(){
+        main();
+    },10);
+})();
+
+function main() {
+    var can_run = true;
+    if (ONLY_WHITE_LIST) {
+        can_run = false;
+        for (var i = 0; i < WHITE_LIST.length; i++) {
+            var site = WHITE_LIST[i];
+            if (Mjztool.matchURL(site)) {
+                can_run = true;
+                break;
+            }
+        }
+    }
+
+    if (can_run) {
+        inputFocus();
+    }
+}
+
+function inputFocus(){
+    if (document.activeElement.tagName != "INPUT") {
+        var inputs = document.body.getElementsByTagName("input");
+        focused = false;
+        for (var i=0; i<inputs.length; i++) {
+            var input = inputs[i];
+            if (elementInViewport(input) && input.type=="text") {
+                input.focus();
+                focused = true;
+                break;
+            }
+        }
+        
+        if (!focused && inputs.length>0)
+          for (var i=0; i<inputs.length; i++) {
+            var input = inputs[i];
+            if (elementInViewport(input)) {
+                input.focus();
+                focused = true;
+                break;
+            }
+        }
+    }
+}
+  
+function elementInViewport(el) {
+    var top = el.offsetTop;
+    var left = el.offsetLeft;
+    var width = el.offsetWidth;
+    var height = el.offsetHeight;
+
+    while(el.offsetParent) {
+        el = el.offsetParent;
+        top += el.offsetTop;
+        left += el.offsetLeft;
+    }
+
+    return (
+        top >= window.pageYOffset &&
+        left >= window.pageXOffset &&
+        (top + height) <= (window.pageYOffset + window.innerHeight) &&
+        (left + width) <= (window.pageXOffset + window.innerWidth)
+    );
+}
+
+// toolkit
+function Mjztool(){}
+Mjztool.bytesToSize = function(bytes) {
+    if (bytes === 0) return '0 B';
+    var k = 1024;
+    sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    i = Math.floor(Math.log(bytes) / Math.log(k));
+    return (bytes / Math.pow(k, i)).toPrecision(3) + ' ' + sizes[i];
+    //toPrecision(3) 后面保留一位小数，如1.0GB                                                                                                                  //return (bytes / Math.pow(k, i)).toPrecision(3) + ' ' + sizes[i];  
+};
+Mjztool.matchURL = function(url) {
+    const URL = window.location.href;
+    return URL.indexOf(url) > -1;
+};
+Mjztool.matchURLAbsolute = function(url) {
+    const href = window.location.href;
+    const len = href.length;
+    return href.indexOf(url) > -1 && url.length == len;
+};
+Mjztool.addStyle = function(styleContent) {
+    var elStyle = document.createElement("style");
+    elStyle.innerHTML = styleContent;
+    document.head.appendChild(elStyle);
+};
+Mjztool.addFunction = function(func, name) {
+    name = name || func.name;
+    document[name] = func;
+};
+Mjztool.GM_setClipboard = function(content) {
+	// @grant        GM_setClipboard
+    GM_setClipboard(content);
+};
+Mjztool.appendScriptLink = function(src) {
+    var f = document.createElement('script');
+    f.src = src;
+    document.body.appendChild(f);
+};
+Mjztool.appendStyleLink = function(src) {
+    var elStyle = document.createElement("link");
+    elStyle.rel="stylesheet";
+    elStyle.type="text/css";
+    elStyle.href = src;
+    document.head.appendChild(elStyle);
+};
+Mjztool.randomSelect = function(list) {
+    var rStart = 0;
+    var rEnd = list.length;
+    var randomIndex = Math.floor(Math.random() * rEnd + rStart);
+    return list[randomIndex];
+};
+Mjztool.filterList = function(list, filter, constructor){
+    if(!(list instanceof Array)) console.error('param "list" is not Array');
+    if(typeof(filter) !== 'function') console.error('param "filter" is not Function');
+
+    var result = [];
+    for (var index = 0; index < list.length; index++) {
+        var element = list[index];
+        if (filter(element, index)) {
+            if(typeof(constructor) === 'function'){
+                result.push(constructor(element, index));
+            }else{
+                result.push(element);
+            }
+        }
+    }
+    return result;
+};
+Mjztool.printIt = function (printThis) {
+	var win = window.open();
+	win.document.open();
+	win.document.write('<html><body style="white-space: pre;">');
+	win.document.write(printThis);
+	win.document.write('</body></html>');
+	win.document.close();
+	win.print();
+	win.close();
+};
